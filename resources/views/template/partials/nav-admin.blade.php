fbf85aee6149a70d5e6f7c15cb150a27330fa3a7
melinopski <melinopski@gmail.com>
2017-08-21 21:36:41 -0500

    login funcional

---
+++ resources/views/template/partials/nav-admin.blade.php
@@ -0,0 +1,18 @@
+<nav class="navbar">
+  		<div >
+    		<div class="navbar-header">
+    			<div class="">
+    				<a class="navbar-brand brand" href="#">
+      					<span class="fa fa-heartbeat fa-2x icono-blanco"  aria-hidden="true"></span>
+      				</a>
+    			</div>
+    		</div>
+
+      @if(Auth::guard('admin')->check())
+      <ul class="nav navbar-nav navbar-right">
+      			<li><a href="{{ route('admin.logout')}}" style=""><span class="fa fa-sign-out fa-2x icono-blanco "></span>
+      			{{ Auth::user()->name}}</a></li>
+    	</ul>
+      @endif
+    </div>
+</nav>
